e3092e2fcb8e41aba57c5e347f224972b90a87f4
Roman Koff <krv@aanet.ru>
2022-03-21 08:52:09 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OpenSiteCore.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
